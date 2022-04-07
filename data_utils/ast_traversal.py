ab0229cfc6aa1ed020055ecff5235ae9cb526a2e
aaajeong <qkrdkwjd59@naver.com>
2022-04-07 12:11:16 +0900

    Modify get SBT method & config file for model(deepcom, h-deepcom, park-deepcom)

--- data_utils/ast_traversal.py
+++ data_utils/ast_traversal.py
@@ -17,6 +17,26 @@ def SBT_(cur_root_id, node_list):
     tmp_list.append(str)
     return tmp_list
 
+def SBTCODE_(cur_root_id, node_list):
+    cur_root = node_list[cur_root_id]
+    tmp_list = []
+    tmp_list.append("(")
+
+    str = cur_root['type']
+    tmp_list.append(str)
+    
+    if 'value' in cur_root:
+        code = cur_root['value']
+        tmp_list.append(code)
+
+    if 'children' in cur_root:
+        chs = cur_root['children']
+        for ch in chs:
+            tmp_list.extend(SBTCODE_(ch, node_list))
+    tmp_list.append(")")
+    tmp_list.append(str)
+    return tmp_list
+
 
 
 def get_sbt_structure(ast_file, out_file):
@@ -29,4 +49,16 @@ def get_sbt_structure(ast_file, out_file):
                 out.write(' '.join(ast_sbt) + '\n')
 
 
-get_sbt_structure("test/test_ast.json", "test.token.ast")
+def get_sbtcode_structure(ast_file, out_file):
+    with open(ast_file, 'r') as ast_file:
+        with open(out_file, 'w+') as out:
+            asts = ast_file.readlines()
+            for a in asts:
+                a = json.loads(a)
+                ast_sbt = SBTCODE_(0, a)
+                out.write(' '.join(ast_sbt) + '\n')
+
+
+# get_sbt_structure("test/test_ast.json", "test.token.ast")
+
+get_sbtcode_structure("ast.json", "sbtcode.ast")
