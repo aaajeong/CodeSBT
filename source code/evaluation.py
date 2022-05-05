9415affb2c24e5102b3cec6527a627337e1d5a8d
aaajeong <qkrdkwjd59@naver.com>
2022-05-05 20:27:45 +0900

    Add METEOR evaluation

--- source code/evaluation.py
+++ source code/evaluation.py
@@ -1,5 +1,8 @@
 import nltk
 from nltk.translate.bleu_score import SmoothingFunction
+from nltk.translate.meteor_score import meteor_score
+nltk.download('wordnet')
+nltk.download('omw-1.4')
 
 def nltk_sentence_bleu(hypotheses, references, order=4):
     refs = []
@@ -19,3 +22,23 @@ def nltk_sentence_bleu(hypotheses, references, order=4):
     avg_score = total_score / count
     print('avg_score: %.4f' % avg_score)
     return avg_score
+
+# def nltk_sentence_bleu(hypotheses, references, order=4):
+#     refs = []
+#     count = 0
+#     total_score = 0.0
+#     # cc = SmoothingFunction()
+#     for hyp, ref in zip(hypotheses, references):
+#         hyp = hyp.split()
+#         ref = ref.split()
+#         refs.append([ref])
+#         if len(hyp) < order:
+#             continue
+#         else:
+#             # score = nltk.translate.bleu([ref], hyp, smoothing_function=cc.method4)
+#             score = meteor_score([ref], hyp)
+#             total_score += score
+#             count += 1
+#     avg_score = total_score / count
+#     print('avg_score: %.4f' % avg_score)
+#     return avg_score
